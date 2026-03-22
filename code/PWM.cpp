28a3905b7fb3b5c20ca7d1fc7ba940c504840411
cub-alan <holwill.jacob@gmail.com>
2026-03-22 21:04:49 +0000

    pwm set up

--- code/PWM.cpp
+++ code/PWM.cpp
@@ -1,2 +1,27 @@
 //
 //
+
+
+void SetUp_PWM(){
+  pwm.begin(); // sets up the PWM for the board
+
+  pwm.setOscillatorFrequency(25000000); // board internal oscillator is between 23-27MHz so 25MHz creates the lowest error interval without having to calibrate to the exact oscillator value
+  pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
+
+  delay(10); // add a debouncing delay
+}
+
+void moveServo(int servo_ARM, float angle) {
+    if (angle < 0){ // check the lower bounds of the servo angle
+       angle = 0; // set the angle to 0 
+       } 
+    else if (angle > 180) { // check the upper bound of the servo angle
+      angle = 180; // set the angle to 180
+      }
+    
+    int pulse_length = map(angle, 0, 180, MID_SERVOMIN, MID_SERVOMAX); // conver the angle to a pulse length
+
+    pwm.setPWM(servo_ARM, 0, pulse_length); // move the servo to that angle
+    delay(500); // add a half a second delay to allow arm to stop in place before moving again
+}
+
--- code/PWM.hpp
+++ code/PWM.hpp
@@ -3,6 +3,28 @@
 #ifndef PWM_HPP
 #define PWM_HPP
 
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();//sets the i2c adress to the boards defualt 
+#define MID_SERVOMIN  150 // This is the 'minimum' pulse length count (out of 4096)
+#define MID_SERVOMAX  600 // This is the 'maximum' pulse length count (out of 4096)
+#define MID_USMIN  600 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
+#define MID_USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
+
+#define SMALL_SERVOMIN  150 // This is the 'minimum' pulse length count (out of 4096)
+#define SMALL_SERVOMAX  600 // This is the 'maximum' pulse length count (out of 4096)
+#define SMALL_USMIN  600 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
+#define SMALL_USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
+
+#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+
+uint8_t Servo_Pan = 0; // int used to set the servo for the first arm joint
+uint8_t Servo_Tilt = 1; // int used to set the servo for the second arm joint
+uint8_t Servo_Cam1 = 2; // int used to set the servo for the third arm joint
+
+void SetUp_PWM();
+
 
 
 #endif
